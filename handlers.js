4259e7573fc2bdaffecb78ad811655be7b9ae257
ALIANE Abdelouahab <abdelouahab_al@yahoo.fr>
2014-11-12 01:30:06 +0100

    Create handlers.js

---
+++ handlers.js
@@ -0,0 +1,11 @@
+exports.index = function (req, res) {
+    res.send('yahala!')
+}
+
+exports.hey = function (req, res) {
+    res.send('hey there! wesh rakoum!')
+}
+
+exports.walou = function (req, res) {
+    res.send('makayen walou!')
+}
