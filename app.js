8aaa6031dde94058c613559eabd9ad6386a7e376
ALIANE Abdelouahab <abdelouahab_al@yahoo.fr>
2014-11-12 03:57:43 +0100

    made the port as variable.
    now, the port is loaded as 'a pro' like tornado do ;)

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.get('/', routes.index)
 app.get('/hey', routes.hey)
 app.get('*', routes.walou)
 
-var server = app.listen(3000, function () {
-    console.log('eh bah voila sa marche au port 3000')
+app.set('port', process.env.PORT || 3000)
+
+var server = app.listen(app.get('port'), function () {
+    console.log('eh bah voila sa marche au port ' + app.get('port'))
 })
